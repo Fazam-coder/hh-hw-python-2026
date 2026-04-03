27ee80b4fc100050ea2d7897610936a08ec7b49a
Fazam-coder <amirrf06@gmail.com>
2026-04-03 08:57:44 +0300

    code

--- app/switchboard.py
+++ app/switchboard.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+from app.users import LocalUser, ForeignUser
 from app.users import User
 
 
@@ -21,6 +22,7 @@ class ActiveCall:
 class Switchboard:
     def __init__(self) -> None:
         self._active_calls: list[ActiveCall] = []
+        self._cross_border_calls: list[ActiveCall] = []
 
     def register_call(self, raw_call: str) -> ActiveCall:
         '''
@@ -29,10 +31,25 @@ class Switchboard:
 
         Например: "1001,Иван Петров,+71234567890,1085,Адам Яковлев,+71255556666"
         '''
-        pass  # Удалите `pass` и пишите ваш код
+        caller_id, caller_name, caller_phone, receiver_id, receiver_name, receiver_phone = raw_call.split(",")
+        caller_id = int(caller_id)
+        receiver_id = int(receiver_id)
+        if caller_phone[:len(LOCAL_PHONE_PREFIX)] == LOCAL_PHONE_PREFIX:
+            caller = LocalUser(caller_id, caller_name, caller_phone)
+        else:
+            caller = ForeignUser(caller_id, caller_name, caller_phone)
+        if receiver_phone[:len(LOCAL_PHONE_PREFIX)] == LOCAL_PHONE_PREFIX:
+            receiver = LocalUser(receiver_id, receiver_name, receiver_phone)
+        else:
+            receiver = ForeignUser(receiver_id, receiver_name, receiver_phone)
+        active_call = ActiveCall(caller, receiver)
+        self._active_calls.append(active_call)
+        if active_call.is_cross_border:
+            self._cross_border_calls.append(active_call)
+        return active_call
 
     def get_active_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return len(self._active_calls)
 
     def get_cross_border_calls_count(self) -> int:
-        pass  # Удалите `pass` и пишите ваш код
+        return len(self._cross_border_calls)
